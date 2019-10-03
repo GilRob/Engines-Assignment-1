460ee8a8351981b3c5550be4aafe70cf20bd47e7
Jessica Le <jessicale422@gmail.com>
2019-10-03 19:46:04 -0400

    Menu Screen
    using singleton

---
+++ LevelEditor/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public Text valueTxt;
+
+    private void Start()
+    {
+            valueTxt.text = PresistentManager.Instance.value.ToString();
+    }
+    public void GoToFirstScene()
+    {
+        SceneManager.LoadScene("UI");
+        PresistentManager.Instance.value++;
+    }
+    public void GoToSecondScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+        PresistentManager.Instance.value++;
+    }
+}
