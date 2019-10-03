af16654efb0f13b5e73a4d72523d715a5aa7a2a5
Gil <gil.robern@ontariotechu.net>
2019-10-03 17:58:32 -0400

    Tried to fix movement
    could not

--- LevelEditor/Assets/Scripts/Movement.cs
+++ LevelEditor/Assets/Scripts/Movement.cs
@@ -12,6 +12,8 @@ public class Movement : MonoBehaviour
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
+    public Camera camera;
+
     public void Start()
     {
         
@@ -44,6 +46,8 @@ public class Movement : MonoBehaviour
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        camera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //transform.Rotate(new Vector3(0.0f, yaw, 0.0f));
     }
 }
